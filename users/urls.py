c0c92483a9a9bb8426ad4f685c3668d5ddc86f55
AllandoAlmeida <alandoalmeida0803@7j50z3.onmicrosoft.com>
2024-01-28 12:17:41 -0300

    feat: app admin

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('logar/', views.logar, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path("create_super_user/", views.create_super_user, name="create_super_user"),
+    path("register/", views.register, name="register"),
+    path("logar/", views.logar, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
@@ -24,12 +25,55 @@ def register(request):
             return redirect("/users/register")
         try:
             User.objects.create_user(username=username, password=password)
-            return redirect("/users/login")
+            return redirect("/users/logar")
         except:
             messages.add_message(
-                request, constants.ERROR, " Erro Interno, refaza a solicitação"
+                request, constants.ERROR, " Erro Interno, refaça a solicitação"
             )
             return redirect("/users/register")
+
+
+def create_super_user(request):
+    if request.method == "GET":
+        return render(request, "create_super_user.html")
+    elif request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+        is_staff = request.POST.get("is_staff")
+
+        if not password == confirm_password:
+            messages.add_message(request, constants.ERROR, " senhas não coíncidem")
+            return redirect("/users/create_super_user")
+
+        user = User.objects.filter(username=username)
+        if user.exists():
+            messages.add_message(request, constants.ERROR, " Usuários já existe")
+            return redirect("/users/create_super_user")
+        # ...
+
+    try:
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            username=username,
+            password=password,
+            is_staff=is_staff,
+        )
+
+        messages.add_message(
+            request, constants.SUCCESS, "Usuário cadastrado com sucesso"
+        )
+        return redirect("/users/create_super_user")
+    except Exception as e:
+        messages.add_message(
+            request, constants.ERROR, "Erro Interno, refaça a solicitação"
+        )
+        return redirect("/users/create_super_user")
 
 
 def logar(request):
